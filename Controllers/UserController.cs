27a8324423a4b5fd28283fd5eb2109b741056bc2
crsoares <crysthianophp@gmail.com>
2019-01-10 15:41:21 -0300

    Criei o controller user e adicionei o metodo get para listar todos os usuarios.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Dapper;
+using Project.Models;
+
+namespace Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<User> Get([FromServices]IConfiguration configuration)
+        {
+            using (SqlConnection conexao = new SqlConnection(configuration.GetConnectionString("Project")))
+            {
+                return conexao.Query<User>("SELECT * FROM dbo.users");
+            }
+        }
+    }
+}
