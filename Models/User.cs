53b21142c9856a1bad5dfc8f34f22e13c8b70e06
crsoares <crysthianophp@gmail.com>
2019-01-10 16:21:17 -0300

    Adicionei o nome da tabela dbo.users e ExplicitKey para identificação da chave primaria.

--- Models/User.cs
+++ Models/User.cs
@@ -1,9 +1,12 @@
 using System;
+using Dapper.Contrib.Extensions;
 
 namespace Project.Models
 {
+    [Table("dbo.users")]
     public class User
     {
+        [ExplicitKey]
         public int id { get; set; }
         public string name { get; set; }
         public string email { get; set; }
