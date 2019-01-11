2fe0b924fabf760819a47420636d2f04cf76bcff
crsoares <crysthianophp@gmail.com>
2019-01-11 19:43:39 -0300

    Declarei o metodo findByEmail.

--- Repositories/IUserRepository.cs
+++ Repositories/IUserRepository.cs
@@ -1,9 +1,10 @@
+using Project.Models;
 using Project.Contracts;
 
 namespace Project.Repositories
 {
     public interface IUserRepository : IRepository
     {
-        
+        User findByEmail(string email);
     }
 }
