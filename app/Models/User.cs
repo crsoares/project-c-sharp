178c4a5bc8a0cef71017cce09a7a48b807f32105
crsoares <crysthianophp@gmail.com>
2019-01-16 16:36:25 -0300

    User atualizado para integração IdentityUser

--- app/Models/User.cs
+++ app/Models/User.cs
@@ -4,5 +4,9 @@ namespace Project.Models
 {
     public class User : IdentityUser
     {
+        public override string PasswordHash {
+            get => base.PasswordHash;
+            set => base.PasswordHash = value;
+        }
     }
 }
